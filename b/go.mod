eb950b7b06d18444c75101ad05322c532901b1d1
Harold Cheng <niuchangcun@gmail.com>
2024-01-03 01:46:22 +0800

    b 1.0

---
+++ b/go.mod
@@ -0,0 +1,5 @@
+module github.com/cjc7373/go-module-test/b
+
+go 1.21.5
+
+require github.com/cjc7373/go-module-test/d v1.1.0
---
+++ b/pkg.go
@@ -0,0 +1,9 @@
+package b
+
+import (
+	"github.com/cjc7373/go-module-test/d"
+)
+
+func ForFloat() float32 {
+	return d.What()
+}
